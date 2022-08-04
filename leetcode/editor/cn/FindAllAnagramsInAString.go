f6a2d7fe73fc8147282efe59f843a03a0ee8d474
alex <lantlot@gmail.com>
2022-08-04 21:17:10 +0800

    1403 11 438

---
+++ leetcode/editor/cn/FindAllAnagramsInAString.go
@@ -0,0 +1,70 @@
+package main
+
+//给定两个字符串 s 和 p，找到 s 中所有 p 的 异位词 的子串，返回这些子串的起始索引。不考虑答案输出的顺序。
+//
+// 异位词 指由相同字母重排列形成的字符串（包括相同的字符串）。
+//
+//
+//
+// 示例 1:
+//
+//
+//输入: s = "cbaebabacd", p = "abc"
+//输出: [0,6]
+//解释:
+//起始索引等于 0 的子串是 "cba", 它是 "abc" 的异位词。
+//起始索引等于 6 的子串是 "bac", 它是 "abc" 的异位词。
+//
+//
+// 示例 2:
+//
+//
+//输入: s = "abab", p = "ab"
+//输出: [0,1,2]
+//解释:
+//起始索引等于 0 的子串是 "ab", 它是 "ab" 的异位词。
+//起始索引等于 1 的子串是 "ba", 它是 "ab" 的异位词。
+//起始索引等于 2 的子串是 "ab", 它是 "ab" 的异位词。
+//
+//
+//
+//
+// 提示:
+//
+//
+// 1 <= s.length, p.length <= 3 * 10⁴
+// s 和 p 仅包含小写字母
+//
+//
+// Related Topics 哈希表 字符串 滑动窗口 👍 962 👎 0
+
+func main() {
+}
+
+// leetcode submit region begin(Prohibit modification and deletion)
+func findAnagrams(s string, p string) []int {
+	if len(s) < len(p) {
+		return nil
+	}
+	lp := len(p)
+	ans := []int{}
+	dic := [26]int{}
+	count := [26]int{}
+	for j, i := range p {
+		dic[i-'a']++
+		count[s[j]-'a']++
+	}
+	if count == dic {
+		ans = append(ans, 0)
+	}
+	for i := 0; i < len(s)-lp; i++ {
+		count[s[i]-'a']--
+		count[s[i+lp]-'a']++
+		if count == dic {
+			ans = append(ans, i+1)
+		}
+	}
+	return ans
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
